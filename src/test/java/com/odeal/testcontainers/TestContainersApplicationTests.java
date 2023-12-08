a56a02f2cb7185707ba5c7afb758ac1bc3f580cc
huseyin <husseyn.mutl@gmail.com>
2023-12-08 09:28:22 +0300

    Initial commit

---
+++ src/test/java/com/odeal/testcontainers/TestContainersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odeal.testcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestContainersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
